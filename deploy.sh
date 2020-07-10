01fcf639ad58a4705cff17a0c58274723b00d5dc
Jan Schill <janschill@me.com>
2020-07-11 00:40:22 +0200

    Use predefined commit message

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
 # Go To Public folder
 cd docs
@@ -15,11 +15,7 @@ cd docs
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+git commit -m "Adding generated files"
 
 # Push source and build repos.
 git push origin master
