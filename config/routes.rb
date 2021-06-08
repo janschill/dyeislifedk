a27cb3c8688c6d2c1d30177d561e7bb3daa31fc5
Jan Schill <janschill@me.com>
2021-06-08 22:50:39 +0200

    Render some partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get "/tournaments", to: "tournaments#index"
+  root 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
