1b6a6a263ae007b4a1b3e0f229ef7867df48377f
Jan Schill <schill@hey.com>
2021-06-09 15:30:21 +0200

    Add more pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get "/about", to: "about#index"
   get "/rules", to: "rules#index"
   get "/tournaments", to: "tournaments#index"
   root 'home#index'
