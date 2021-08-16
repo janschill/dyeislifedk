2cdd74e8017c0130662f660b65105533dbac3e32
Jan Schill <schill@hey.com>
2021-08-16 13:19:30 +0200

    Remove Solidus

--- db/schema.rb
+++ db/schema.rb
@@ -38,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_06_14_131815) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
